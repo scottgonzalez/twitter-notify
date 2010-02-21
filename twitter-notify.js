2a555174560967badc81dd9c52e256ac843e18ef
Scott González <scott.gonzalez@gmail.com>
2010-02-20 21:51:10 -0500

    Read search term from standard input. Adds support for multiple search terms.

--- twitter-notify.js
+++ twitter-notify.js
@@ -2,23 +2,27 @@ var http = require( "http" ),
 	QueryString = require( "QueryString" ),
 	Growl = require( "./lib/Growl" ).Growl;
 
-var search = "ncjs",
-	since = 0;
+function addSearch( term ) {
+	getTweets({ term: term, since: 0 });
+}
 
-function getTweets() {
+function getTweets( search ) {
 	var query = QueryString.stringify({
-		q: search,
-		since_id: since
+		q: search.term,
+		since_id: search.since
 	});
 	http.cat( "http://search.twitter.com/search.json?" + query, function( status, content ) {
 		JSON.parse( content ).results.forEach(function( tweet ) {
-			if ( tweet.id > since) {
-				since = tweet.id;
+			if ( tweet.id > search.since) {
+				search.since = tweet.id;
 			}
 			Growl.notify( "From " + tweet.from_user + ": " + tweet.text );
 		});
-		setTimeout( getTweets, 10000 );
+		setTimeout( getTweets, 10000, search );
 	});
 }
 
-getTweets();
+process.stdio.open();
+process.stdio.addListener( "data", function( input ) {
+	addSearch( input.trim() );
+});
