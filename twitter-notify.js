04db7801b932440554a9561636e8a7e65697705d
Brendan Blackwood <bblackwo@bblackwo-mac.broadation>
2010-02-23 11:13:44 -0500

    Added input parsing to check allow for adding, removing and listing current searches.

--- twitter-notify.js
+++ twitter-notify.js
@@ -8,5 +8,21 @@ process.argv.splice( 2 ).forEach( TwitterNotify.addSearch );
 // listen to stdin
 process.stdio.open();
 process.stdio.addListener( "data", function( input ) {
-	TwitterNotify.addSearch( input.trim() );
+	var command = input.trim();
+	switch (command[0]) {
+		case "l":
+			require("sys").puts( "Current Search Terms:" );
+			for (var term in TwitterNotify.searches) {
+			    require("sys").puts( "\t"+term );
+			}
+			break;
+		case "-":
+			TwitterNotify.removeSearch( command.substring(1) );
+			break;
+		case "+":
+			TwitterNotify.addSearch( command.substring(1) );
+			break;
+		default:
+	}
+	
 });
