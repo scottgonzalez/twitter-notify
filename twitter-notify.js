ea8016bd234b19d9ac7423e4575943d522f0f9ea
Scott González <scott.gonzalez@gmail.com>
2010-02-22 22:36:14 -0500

    Accept command line arguments as search terms.

--- twitter-notify.js
+++ twitter-notify.js
@@ -2,6 +2,9 @@ var TwitterNotify = require( "./lib/TwitterNotify" );
 
 TwitterNotify.init();
 
+// add any additional command line args as search terms
+process.argv.splice( 2 ).forEach( TwitterNotify.addSearch );
+
 // listen to stdin
 process.stdio.open();
 process.stdio.addListener( "data", function( input ) {
